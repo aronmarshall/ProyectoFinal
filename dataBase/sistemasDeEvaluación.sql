d168e19fdbb2f9964a7a5f1737dffb12a9dfcec8
root <root@DESKTOP-DGTFSMP>
2024-04-24 19:12:25 -0600

    Primera versión de la base de datos, incluye usuario y alumnos.

---
+++ dataBase/sistemasDeEvaluación.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE sistemaDeEvaluacionCodigo;
+USE sistemaDeEvaluacionCodigo;
+
+CREATE TABLE alumnos (
+    idAlumno INT NOT NULL AUTO_INCREMENT, 
+    nombreCompleto VARCHAR (40) NOT NULL,
+    matricula VARCHAR (10) NOT NULL, 
+    usuario VARCHAR (20) NOT NULL,
+    contrasenia VARBINARY (64) NOT NULL,
+    PRIMARY KEY (idAlumno)
+    );
+CREATE TABLE usuarios (
+    idRegistro  INT NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR (20) NOT NULL,
+    contrasenia VARBINARY (64) NOT NULL,
+    FOREIGN KEY (idRegistro) REFERENCES alumnos(idAlumno)
+);
