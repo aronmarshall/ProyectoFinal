7e33c2ffc9bffb22f6c07564bffa744414cddec9
root <root@DESKTOP-DGTFSMP>
2024-05-29 20:31:51 -0600

    versión final del bot.

--- systemEvaluacion/api/telegram.py
+++ systemEvaluacion/api/telegram.py
@@ -12,22 +12,28 @@ bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def mensaje_bienvenida(message:str):
+def mensaje_bienvenida(message:str)->None:
     """
     Maneja los comandos /start y /help y envía un mensaje de bienvenida al usuario.
 
     Args:
         message: El mensaje recibido que contiene el comando.
+    
+    Returns:
+        None
     """
     bot.reply_to(message, "Hola! Usa /solicitar_token para obtener un token o /my_user ")
 
 @bot.message_handler(commands=['solicitar_token'])
-def solicitar_token_handler(message:str):
+def solicitar_token_handler(message:str)->None:
     """
     Maneja el comando /solicitar_token y envía un token OTP al usuario.
 
     Args:
         message: El mensaje recibido que contiene el comando.
+    
+    Returns:
+        None
     """
     usuario_o_telefono = message.chat.id  
     username = message.from_user.username
@@ -36,7 +42,16 @@ def solicitar_token_handler(message:str):
     mensaje = f"# {token}"
     enviar_token_usuario(usuario_o_telefono, mensaje)
 
-def existe_token_de_usuario(username):
+def existe_token_de_usuario(username:str)->str:
+    """
+        Verifica si existe un token válido para el nombre de usuario dado.
+
+    Args:
+        username (str): El nombre de usuario a verificar.
+
+    Returns:
+        str: Si existe un token válido, devuelve el token. Si no, devuelve un mensaje indicando que el usuario no ha iniciado el proceso de registro.
+    """
     token_valido = models.TelegramData.objects.filter(usuarioTelegram=username, enviar=True).values_list('tokens', flat=True).first()
     if not token_valido:
             token_enviado = models.TelegramData.objects.filter(usuarioTelegram=username, enviar=False).order_by('-tiempo').values_list('tokens', flat=True).first()
@@ -49,13 +64,16 @@ def existe_token_de_usuario(username):
         models.TelegramData.objects.filter(tokens=token_valido, enviar=True).update(enviar=False)
         return token_valido
 
-def enviar_token_usuario(usuario_o_telefono, mensaje) -> None:
+def enviar_token_usuario(usuario_o_telefono, mensaje)->None:
     """
     Envía el token OTP al usuario a través de la API de Telegram.
 
     Args:
         usuario_o_telefono: El nombre de usuario o ID del chat de Telegram.
         mensaje: El mensaje que contiene el token OTP.
+    
+    Returns:
+        None
     """
     try:
         bot.send_message(usuario_o_telefono, mensaje)
@@ -64,12 +82,27 @@ def enviar_token_usuario(usuario_o_telefono, mensaje) -> None:
 
 @bot.message_handler(commands=['my_user'])
 def enviar_usuario(message:str)-> str:
+    """
+        Envía al usuario su nombre de usuario.
+
+    Args:
+        message (str): El mensaje recibido por el bot.
+
+    Returns:
+        str: El nombre de usuario del usuario que envió el mensaje.
+    """
     username = message.from_user.username
     bot.reply_to(message, f"Tu usuario es: {username}")
     
+def run_bot()->None:
+    """
+        Inicia la ejecución del bot en un hilo separado.
 
+    Esta función utiliza el método de polling para recibir actualizaciones del bot y lo ejecuta en un hilo separado de forma demoníaca.
 
-def run_bot():
+    Returns:
+        None
+    """
     bot.polling()
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.daemon = True
