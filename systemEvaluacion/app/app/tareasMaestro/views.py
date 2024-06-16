1c8bb36807ebf5c4afdf40109e78733b9ee6e6a5
cindyld <cindylanda21@hotmail.com>
2024-06-16 19:47:28 -0400

    tareasMaestro desc

--- systemEvaluacion/app/app/tareasMaestro/views.py
+++ systemEvaluacion/app/app/tareasMaestro/views.py
@@ -18,16 +18,25 @@ import secrets
 import string
 import random
 
-
 def crear_tarea(request):
+    """
+    Crea una nueva tarea basada en los datos proporcionados por el formulario POST.
+    
+    Args:
+        request: Objeto HttpRequest que contiene metadatos sobre la solicitud.
+        
+    Returns:
+        HttpResponse: Renderiza la página de inicio del maestro después de crear la tarea.
+    """
     if not request.session.get('maestro'):
-        return redirect('/inicio')
+        return redirect('/inicio') 
     else:
         if request.method == 'GET':
-            return render(request, 'crear_tarea.html')
+            return render(request, 'crear_tarea.html')  
         elif request.method == 'POST':
+            # Obtiene datos del formulario y genera un id de tarea
             cadena_numeros = nrc()
-            id_tarea=cadena_numeros
+            id_tarea = cadena_numeros
             nombre_eje = request.POST.get('nombre_eje')
             descripcion = request.POST.get('descripcion')
             fecha_inicio = request.POST.get('fecha_inicio')
@@ -36,38 +45,70 @@ def crear_tarea(request):
             salida = request.POST.get('salida')
             salida_ejemplo = request.FILES.get('salida_ejemplo')
 
-            insertar_datos = models.crear_tarea(id_tarea=id_tarea, nombre=nombre_eje, descripcion_general=descripcion, 
-                fecha_inicio=fecha_inicio, fecha_cierre=fecha_cierre, entrada_esperada=entrada, 
-                salida_esperada=salida, archivo_evaluar=salida_ejemplo)
+            # Inserta los datos de la nueva tarea en la base de datos
+            insertar_datos = models.crear_tarea(
+                id_tarea=id_tarea, 
+                nombre=nombre_eje, 
+                descripcion_general=descripcion, 
+                fecha_inicio=fecha_inicio, 
+                fecha_cierre=fecha_cierre, 
+                entrada_esperada=entrada, 
+                salida_esperada=salida, 
+                archivo_evaluar=salida_ejemplo
+            )
             insertar_datos.save()
 
-            messages.info(request, f'Se inserto la actividad {nombre_eje}')
+            messages.info(request, f'Se insertó la actividad {nombre_eje}')
             return render(request, 'inicio_maestro.html')
 
 def nrc():
+    """
+    Genera un número de referencia de 4 dígitos.
+    
+    Returns:
+        str: Número de referencia de 4 dígitos.
+    """
     return ''.join(str(random.randint(0, 9)) for _ in range(4))
 
 def listar_mis_tareas(request):
+    """
+    Lista todas las tareas creadas por el maestro.
+    
+    Args:
+        request: Objeto HttpRequest que contiene metadatos sobre la solicitud.
+        
+    Returns:
+        HttpResponse: Renderiza la página con la lista de tareas del maestro.
+    """
     if not request.session.get('maestro'):
-        return redirect('/inicio')
+        return redirect('/inicio')  
     else:
         if request.method == 'POST':
             return render(request, 'listar_mis_tareas.html')
         elif request.method == 'GET':
-            consultar_mis_tareas = models.crear_tarea.objects.all()
+            consultar_mis_tareas = models.crear_tarea.objects.all()  # Obtiene todas las tareas del maestro
             messages.info(request, f'Se encontraron {consultar_mis_tareas.count()} tareas.')
             return render(request, 'listar_mis_tareas.html', {'tareas': consultar_mis_tareas})
 
 def eliminar_tareas(request):
+    """
+    Elimina una tarea específica basada en el ID proporcionado por el formulario POST.
+    
+    Args:
+        request: Objeto HttpRequest que contiene metadatos sobre la solicitud.
+        
+    Returns:
+        HttpResponse: Redirige a la página con la lista de tareas después de intentar eliminar una tarea.
+    """
     if not request.session.get('maestro'):
-        return redirect('/inicio')
+        return redirect('/inicio')  
     else:
         if request.method == 'GET':
             return render(request, 'listar_mis_tareas.html')
         elif request.method == 'POST':
             eliminar = request.POST.get('elimina_tarea')
             try:
-                eliminar_numero = int(eliminar)  
+                eliminar_numero = int(eliminar)  # Convierte el ID a un número entero
             except ValueError:
                 messages.error(request, 'Por favor, ingresa solo números enteros.')
                 return redirect('/listar_mis_tareas')
@@ -78,22 +119,37 @@ def eliminar_tareas(request):
             else:
                 try:
                     tarea = models.crear_tarea.objects.get(id_tarea=eliminar_numero)
-                    tarea.delete()
-                    messages.success(request, f'Tarea "{tarea.nombre}" eliminada correctamente.')            
+                    tarea.delete()  # Elimina la tarea de la base de datos
+                    messages.success(request, f'Tarea "{tarea.nombre}" eliminada correctamente.')
                     return redirect('/listar_mis_tareas')
                 except models.crear_tarea.DoesNotExist:
                     messages.error(request, f'La tarea "{eliminar_numero}" no existe.')
                     return redirect('/listar_mis_tareas')
 
 def existe_ejercicio(eliminar):
-    existe=models.crear_tarea.objects.filter(id_tarea=eliminar).exists()
-    if existe:
-        return True
-    else:
-        return False
+    """
+    Verifica si una tarea con el ID especificado existe.
+    
+    Args:
+        eliminar: ID de la tarea a verificar.
+        
+    Returns:
+        bool: True si la tarea existe, False en caso contrario.
+    """
+    existe = models.crear_tarea.objects.filter(id_tarea=eliminar).exists()
+    return existe
 
 def respuestas_estudiantes(request):
+    """
+    Renderiza la página para mostrar las respuestas de los estudiantes.
+    
+    Args:
+        request: Objeto HttpRequest que contiene metadatos sobre la solicitud.
+        
+    Returns:
+        HttpResponse: Renderiza la página de respuestas de los estudiantes.
+    """
     if not request.session.get('maestro'):
-        return redirect('/inicio')
+        return redirect('/inicio')  
     else:
         return render(request, 'listar_mis_tareas.html')
